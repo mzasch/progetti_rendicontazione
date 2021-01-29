6b6a609e2b4250860325dc0b36525d323d6d27cb
mcaceffo <mcaceffo@chilesotti.it>
2021-01-29 15:35:52 +0100

    Refactoring del render dei progetti tra FS e referente

---
+++ reportProgetto.php
@@ -0,0 +1,48 @@
+<?php
+    session_start();
+
+    function renderProgetto($progettoId) {
+        require('env.php');
+        $connection = mysqli_connect($host, $user, $password, $dbname)
+                      or die('Something went horribly wrong with the connection' . mysqli_connect_error());
+
+        $progetto = "SELECT CONCAT_WS(' ', rd_rend.Cognome, rd_rend.Nome) AS docente, ".
+                      "ro.dataOra, ro.nOre, ro.tipologiaOre ".
+                      "FROM `rend_orerendicontate` ro ".
+                      "JOIN `rend_docenti` rd_rend ON ro.docente = rd_rend.id ".
+                      "JOIN `rend_progetti` rp ON ro.progetto = rp.id ".
+                      "WHERE rp.id = $progettoId";
+
+        if(!$ore = mysqli_query($connection, $progetto)) {
+            var_dump($connection);
+            echo "<div class='error'>";
+            echo "<p>Errore nel recupero dei dati del progetto $progettoId</p>";
+            echo "<p>Query: $progetto</p>";
+            echo "<p>Errno: " . $connection -> errno . "</p>";
+            echo "<p>Error: " . $connection -> error . "</p>";
+            echo "</div>";
+            exit;
+        }
+?>
+    <table class="table table-hover table-sm">
+      <thead class="thead-dark">
+        <tr>
+          <th scope="col">Docente</th>
+          <th scope="col">Data/Ora</th>
+          <th scope="col">Ore</th>
+          <th scope="col">Tipologia</th>
+        </tr>
+      </thead>
+      <tbody>
+      <?php foreach($ore as $ora) { ?>
+        <tr>
+          <th scope="row"><?php echo $ora['docente']; ?></th>
+          <td><?php echo $ora['dataOra']; ?></td>
+          <td><?php echo $ora['nOre']; ?></td>
+          <td><?php echo $ora['tipologiaOre']; ?></td>
+        </tr>
+      <?php } ?>
+      </tbody>
+    </table>
+    <hr />
+<?php } ?>
--- reportReferente.php
+++ reportReferente.php
@@ -1,63 +1,43 @@
 <?php
     session_start();
     require_once('env.php');
+    require_once('reportProgetto.php');
+
     if (isset($_SESSION['loggedEmail']) && $_SESSION['loggedEmail']) {
         $currentUser = $_SESSION['loggedEmail'];
         $connection = mysqli_connect($host, $user, $password, $dbname)
                       or die('Something went horribly wrong with the connection' . mysqli_connect_error());
 
-        $query_dati = "SELECT rp.nome_progetto, ro.progetto AS id_progetto, ".
-            "CONCAT_WS(' ', rd_rend.Cognome, rd_rend.Nome) AS docente, ".
-            "ro.dataOra, ro.nOre, ro.tipologiaOre ".
-            "FROM `rend_orerendicontate` ro ".
-            "JOIN `rend_docenti` rd_rend ON ro.docente = rd_rend.id ".
-            "JOIN `rend_progetti` rp ON ro.progetto = rp.id ".
-            "JOIN `rend_docenti` rd_ref ON rp.referente = rd_ref.id ".
-            "WHERE rd_ref.email = '" . $currentUser . "' ".
-            "ORDER BY ro.progetto, ro.docente, ro.dataOra";
+        $query_ref = "SELECT rd.id as refid ".
+                    "FROM rend_docenti rd ".
+                    "WHERE rd.email = '$currentUser'";
 
-        if(!$dati = mysqli_query($connection, $query_dati)) {
-          echo "Something went horribly wrong with the query \"dati\"\n";
-          echo "Errno: " . $connection -> errno . "\n";
-          echo "Error: " . $connection -> error . "\n";
-          exit;
+        if($ref_docente = mysqli_query($connection, $query_fs)) {
+            if($ref_docente -> num_rows != 0){
+                $res = mysqli_fetch_assoc($ref_docente);
+
+                $progetti_ref = "SELECT rp.id, rp.nome_progetto ".
+                              "FROM `rend_progetti` rp ".
+                              "WHERE rp.referente = " . $res["refid"] . " " .
+                              "ORDER BY rp.nome_progetto";
+
+                if(!$progetti = mysqli_query($connection, $progetti_fs)) {
+                    echo "<div class='error'>";
+                    echo "<p>Errore nel recupero dei dati dei progetti</p>";
+                    echo "<p>Errno: " . $connection -> errno . "</p>";
+                    echo "<p>Error: " . $connection -> error . "</p>";
+                    echo "</div>";
+                    exit;
+                }
+            }
         }
-
-        $groupedResult = array();
-        $i = 0;
-        while ($res = mysqli_fetch_assoc($dati)) {
-            $groupedResult[$res['nome_progetto']][$i] = $res;
-            $i++;
-        }
-        ksort($groupedResult);
-    }
-
-    echo "<div id='reports'>\n";
-    foreach($groupedResult as $progetto => $ore) {
 ?>
-<div class='report-progetto'>
-    <h3><?php echo $progetto ?></h3>
-    <table class="table table-hover table-sm">
-      <thead class="thead-dark">
-        <tr>
-          <th scope="col">Docente</th>
-          <th scope="col">Data/Ora</th>
-          <th scope="col">Ore</th>
-          <th scope="col">Tipologia</th>
-        </tr>
-      </thead>
-      <tbody>
-      <?php foreach($ore as $ora) { ?>
-        <tr>
-          <th scope="row"><?php echo $ora['docente'] ?></th>
-          <td><?php echo $ora['dataOra'] ?></td>
-          <td><?php echo $ora['nOre'] ?></td>
-          <td><?php echo $ora['tipologiaOre'] ?></td>
-        </tr>
-      <?php } ?>
-      </tbody>
-    </table>
-    <hr />
-</div>
-<?php } ?>
+
+<div id='reports'>
+    <?php while ($res = mysqli_fetch_assoc($progetti)) { ?>
+    <div class='report-progetto'>
+        <h3><?php echo $res['nome_progetto'] ?></h3>
+        <?php renderProgetto($res['id']); ?>
+    </div>
+    <?php } } ?>
 </div>
