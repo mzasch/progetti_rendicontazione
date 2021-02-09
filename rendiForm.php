7315c8e44fd46528280e28bcd5eab8794d014211
mcaceffo <mcaceffo@chilesotti.it>
2021-02-09 11:31:43 +0100

    Validazione della form

--- rendiForm.php
+++ rendiForm.php
@@ -42,77 +42,68 @@
 
 ?>
 
-                    <form id="form" class="form-horizontal" method="post" action="inviaOre.php">
-<?php /*						<div class="form-group">
-        					<label for="sDocente" class="cols-sm-2 control-label">Scegli il docente:</label>
-							<div class="cols-sm-10">
-								<div class="input-group">
-							        <select id='sDocente' name="docente">
-									<option value='' selected></option>
-									<?php
-							            while($res = mysqli_fetch_assoc($docenti)) {
-							              $cognome = $res['cognome'];
-							              $nome = $res['nome'];
-							              echo "<option value='" . $res['id'] . "'>" . $cognome . " " . $nome . "</option>\n";
-							            }
-							          ?>
-									</select>
-								</div>
-							</div>
-						</div>
-*/ ?>
-						<div class="form-group">
-							<label for="sProgetto" class="cols-sm-2 control-label">Scegli un progetto:</label>
-							<div class="cols-sm-10">
-								<div class="input-group">
-							        <select id='sProgetto' name="progetto">
-									<option value='' selected></option>
-									<?php
-							            while($res = mysqli_fetch_assoc($progetti)) {
-								            $nome_progetto = $res['nome_progetto'];
-									        echo "<option value='" . $res['id'] . "'>" . $nome_progetto . "</option>\n";
-										}
-							        ?>
-							        </select>
-								</div>
-							</div>
-						</div>
+<form id="form" class="form-horizontal needs-validation" method="post" action="inviaOre.php" novalidate>
+	<div class="form-group">
+		<label for="sProgetto" class="cols-sm-2 control-label">Scegli un progetto:</label>
+		<div class="cols-sm-10">
+			<div class="input-group">
+		        <select id='sProgetto' name="progetto" required>
+				<option value='' selected></option>
+				<?php
+		            while($res = mysqli_fetch_assoc($progetti)) {
+			            $nome_progetto = $res['nome_progetto'];
+				        echo "<option value='" . $res['id'] . "'>" . $nome_progetto . "</option>\n";
+					}
+		        ?>
+		        </select>
+                <div class="invalid-feedback">
+                    Seleziona un progetto dall'elenco.
+                </div>
+			</div>
+		</div>
+	</div>
 
-						<div class="form-group">
-							<label for="sData" class="cols-sm-2 control-label">Scegli la data e l'ora d'inizio dell'attivit&agrave;</label>
-							<div class="cols-sm-10">
-								<div class="input-group">
-									<input type="text" class="form-control" name="datapicker" id="sData"/>
-								</div>
-							</div>
-						</div>
+	<div class="form-group">
+		<label for="sData" class="cols-sm-2 control-label">Scegli la data e l'ora d'inizio dell'attivit&agrave;</label>
+		<div class="cols-sm-10">
+			<div class="input-group">
+				<input type="text" class="form-control" name="datapicker" id="sData" required />
+			</div>
+		</div>
+	</div>
 
-						<div class="form-group">
-        					<label for="sOre" class="cols-sm-2 control-label">Inserisci la durata dell'attivit&agrave;</label>
-							<div class="cols-sm-10">
-								<div class="input-group">
-							        <input id="sOre" type = "number" name = "nOre" class="form-control" min='0.5' max='8' step='0.5'/>
-								</div>
-							</div>
-						</div>
+	<div class="form-group">
+		<label for="sOre" class="cols-sm-2 control-label">Inserisci la durata dell'attivit&agrave;</label>
+		<div class="cols-sm-10">
+			<div class="input-group">
+		        <input id="sOre" type = "number" name = "nOre" class="form-control" min='0.5' max='8' step='0.5' required/>
+                <div class="invalid-feedback">
+                    Inserisci il numero di ore svolte.
+                </div>
+            </div>
+		</div>
+	</div>
 
-						<div class="form-group">
-							<label for="sTipoOre" class="cols-sm-2 control-label">Tipo delle ore svolte:</label>
-							<div class="cols-sm-10">
-								<div class="input-group">
-							        <select id='sTipoOre' name="tipoOre" class="form-control">
-                                        <option value='5'>Progettazione - Ore retribuite</option>
-										<option value='6'>Progettazione - Ore in obbligo</option>
-                                        <option value='1'selected>Realizzazione - Docenza retribuita</option>
-                                        <option value='2'>Realizzazione - Docenza in obbligo</option>
-										<option value='3'>Realizzazione - Assistenza/Tutoraggio retribuita</option>
-										<option value='4'>Realizzazione - Assistenza/Tutoraggio in obbligo</option>
-									</select>
-								</div>
-							</div>
-						</div>
+	<div class="form-group">
+		<label for="sTipoOre" class="cols-sm-2 control-label">Tipo delle ore svolte:</label>
+		<div class="cols-sm-10">
+			<div class="input-group">
+		        <select id='sTipoOre' name="tipoOre" class="form-control" required>
+                    <option value='5'>Progettazione - Ore retribuite</option>
+					<option value='6'>Progettazione - Ore in obbligo</option>
+                    <option value='1'selected>Realizzazione - Docenza retribuita</option>
+                    <option value='2'>Realizzazione - Docenza in obbligo</option>
+					<option value='3'>Realizzazione - Assistenza/Tutoraggio retribuita</option>
+					<option value='4'>Realizzazione - Assistenza/Tutoraggio in obbligo</option>
+				</select>
+                <div class="invalid-feedback">
+                    Seleziona la tipologia di ore.
+                </div>
+			</div>
+		</div>
+	</div>
 
-						<div class="form-group ">
-							<input type="submit" class="btn btn-success btn-lg btn-block login-button" value="Registra l'attivit&agrave;" />
-						</div>
-					</form>
+	<div class="form-group">
+		<input type="submit" class="btn btn-success btn-lg btn-block login-button" value="Registra l'attivit&agrave;" />
+	</div>
+</form>
